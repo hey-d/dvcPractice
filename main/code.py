7ef5ce5a864b9fc440567c50d6370a9360d8783b
hey-d <dushyantmanghani@gmail.com>
2026-06-03 12:26:11 +0530

    third commit

--- main/code.py
+++ main/code.py
@@ -11,6 +11,8 @@ dAta = {
 df = pd.DataFrame(dAta)
 df.loc[len(df)]=["satyarth", 21]
 
+df.loc[len(df)]=['somebody', 25]
+
 path = "data/some.csv"
 df.to_csv(path, index=False)
 print("data changed at csv file")
